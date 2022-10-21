756c33ba1e629069520ad03e1f3fff5dc204d772
Tushar Garg <tshrgarg2010@gmail.com>
2022-10-21 20:52:00 +0530

    Add 2 numbers

---
+++ 6.rb
@@ -0,0 +1,15 @@
+#ADD 2 NUMBERS FROM USER
+
+puts("Enter num1")
+a=gets.chomp()
+
+puts("Enter num2")
+b=gets.chomp()
+
+puts(a+b)
+puts(a.to_i+b.to_i)
+
+#default input is string
+
+puts("Enter num")
+c=gets.to_f
